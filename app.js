108520a870aba09e71195df4aaef62fc312d894d
SenaaJonathan <senajonathan01@gmail.com>
2025-03-08 19:03:18 -0300

    obtenemos los elementos de la lista

--- app.js
+++ app.js
@@ -16,10 +16,28 @@ function agregarAmigo() {
             console.log(amigos);
             //Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
             limpiarCaja();
+            ActualizarLista();
         }
     }
 }
  
+
+//Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+function ActualizarLista() {
+    let lista = (document.getElementById("listaAmigos"));
+    //Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+    lista.innerHTML = "";
+//Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+for(let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+//Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+    }
+}
+
+ActualizarLista();
+
 
 function limpiarCaja() {
     document.querySelector("#amigo").value = "";
