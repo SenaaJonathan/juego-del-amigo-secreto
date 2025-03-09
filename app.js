4dbda797ce6bbf4db909cabc865605dcb59ee1e5
SenaaJonathan <senajonathan01@gmail.com>
2025-03-09 14:44:42 -0300

    Funcion para sortear amigo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function agregarAmigo() {
     console.log(amigoAgregado);
     //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
     if (amigoAgregado == "") {
-        alert("¡Por favor, inserte un nombre!");
+        alert("¡Por favor, digite un nombre!");
     //Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().    
     } else {
         if (amigos.includes(amigoAgregado)) {
@@ -30,13 +30,30 @@ function ActualizarLista() {
 //Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
 for(let i = 0; i < amigos.length; i++) {
     let li = document.createElement("li");
+    //Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
     li.textContent = amigos[i];
     lista.appendChild(li);
-//Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
     }
 }
 
 ActualizarLista();
+
+
+//Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert ("No hay amigos disponibles para sortear, ingrese al menos uno.");
+        return;
+    } else {
+        //Generar un índice aleatorio: Usar Math.random() y Math.floor() para seleccionar un índice aleatorio del arreglo.
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        //Obtener el nombre sorteado: Utilizar el índice aleatorio para acceder al nombre correspondiente en el arreglo.
+        let amigoSorteado = amigos[indiceAleatorio];
+        //Mostrar el resultado: Actualizar el contenido del elemento de resultado utilizando document.getElementById()  e innerHTML para mostrar el amigo sorteado.
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+    }
+}
 
 
 function limpiarCaja() {
